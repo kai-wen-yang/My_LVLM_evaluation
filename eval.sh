023284f5b17146086e228863d9375baf3d943a3b
Kaiwen Yang <92202424+kai-wen-yang@users.noreply.github.com>
2023-09-17 11:29:23 +0800

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -2,6 +2,11 @@ export TORCH_HUB=/fs/nexus-scratch/kwyang3/models
 export TRANSFORMERS_CACHE=/fs/nexus-scratch/kwyang3/models
 export HF_DATASETS_CACHE=/fs/nexus-scratch/kwyang3/data
 
+cd /fs/nexus-scratch/kwyang3/My_LVLM_evaluation/models
+export PYTHONPATH="$PYTHONPATH:$PWD"
+
+cd ..
+
 python eval.py --model_name Cheetah --batch_size 16 --eval_cls --dataset_name ImageNet --device 0 --sample_num 1000
 
 #python eval.py --model_name InstructBLIP --batch_size 2 --eval_vqa --dataset_name VCR1_MCI --device 0 --sample_num 1000
