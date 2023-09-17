e3f6551be065f7bd64e68051fc3c33edcb81e07c
Kaiwen Yang <92202424+kai-wen-yang@users.noreply.github.com>
2023-09-17 17:22:29 +0800

    Update test_cheetah.py

--- models/test_cheetah.py
+++ models/test_cheetah.py
@@ -41,7 +41,8 @@ class TestCheetah:
         # context = ["<ImageHere> "+ques for ques in question_list]
         # output = self.chat.batch_answer(raw_img_list, context, max_new_tokens=max_new_tokens)
         outputs = []
-        for image, question in zip(image_list, question_list)
+        for image, question in zip(image_list, question_list):
+            pdb.set_trace()
             output = self.chat.answer([image], question, max_new_tokens=max_new_tokens)
             outputs.append(output)
         return outputs
