6573e0c1f0294c7a98fc73e4425b4b7c35942ecf
Kaiwen Yang <92202424+kai-wen-yang@users.noreply.github.com>
2023-09-17 11:36:11 +0800

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -8,6 +8,7 @@ export PYTHONPATH="$PYTHONPATH:$PWD"
 cd ..
 
 python eval.py --model_name Cheetah --batch_size 16 --eval_cls --dataset_name ImageNet --device 0 --sample_num 1000
+python eval.py --model_name InstructBLIP --batch_size 16 --eval_cls --dataset_name ImageNet --device 0 --sample_num 1000
 
 #python eval.py --model_name InstructBLIP --batch_size 2 --eval_vqa --dataset_name VCR1_MCI --device 0 --sample_num 1000
 
