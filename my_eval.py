42f8d74aaeefc4b5032e3b538a248cb92cb173cb
Kaiwen Yang <92202424+kai-wen-yang@users.noreply.github.com>
2023-09-19 21:18:16 +0800

    Update my_eval.py

--- my_eval.py
+++ my_eval.py
@@ -57,43 +57,14 @@ def parse_args():
     return args
 
 
-def accuracy(output: torch.Tensor, target: torch.Tensor, topk=(1,)) -> List[torch.FloatTensor]:
-    """
-    Computes the accuracy over the k top predictions for the specified values of k
-    In top-5 accuracy you give yourself credit for having the right answer
-    if the right answer appears in your top five guesses.
-
-    ref:
-    - https://pytorch.org/docs/stable/generated/torch.topk.html
-    - https://discuss.pytorch.org/t/imagenet-example-accuracy-calculation/7840
-    - https://gist.github.com/weiaicunzai/2a5ae6eac6712c70bde0630f3e76b77b
-    - https://discuss.pytorch.org/t/top-k-error-calculation/48815/2
-    - https://stackoverflow.com/questions/59474987/how-to-get-top-k-accuracy-in-semantic-segmentation-using-pytorch
-
-    :param output: output is the prediction of the model e.g. scores, logits, raw y_pred before normalization or getting classes
-    :param target: target is the truth
-    :param topk: tuple of topk's to compute e.g. (1, 2, 5) computes top 1, top 2 and top 5.
-    e.g. in top 2 it means you get a +1 if your models's top 2 predictions are in the right label.
-    So if your model predicts cat, dog (0, 1) and the true label was bird (3) you get zero
-    but if it were either cat or dog you'd accumulate +1 for that example.
-    :return: list of topk accuracy [top1st, top2nd, ...] depending on your topk input
-    """
+def accuracy(output: torch.Tensor, target: torch.Tensor, topk=(1,)):
     with torch.no_grad():
-        # ---- get the topk most likely labels according to your model
-        # get the largest k \in [n_classes] (i.e. the number of most likely probabilities we will use)
+
         maxk = max(topk)  # max number labels we will consider in the right choices for out model
         batch_size = target.size(0)
 
-        # get top maxk indicies that correspond to the most likely probability scores
-        # (note _ means we don't care about the actual top maxk scores just their corresponding indicies/labels)
         _, y_pred = output.topk(k=maxk, dim=1)  # _, [B, n_classes] -> [B, maxk]
         y_pred = y_pred.t()  # [B, maxk] -> [maxk, B] Expects input to be <= 2-D tensor and transposes dimensions 0 and 1.
-
-        # - get the credit for each example if the models predictions is in maxk values (main crux of code)
-        # for any example, the model will get credit if it's prediction matches the ground truth
-        # for each example we compare if the model's best prediction matches the truth. If yes we get an entry of 1.
-        # if the k'th top answer of the model matches the truth we get 1.
-        # Note: this for any example in batch we can only ever get 1 match (so we never overestimate accuracy <1)
         target_reshaped = target.view(1, -1).expand_as(y_pred)  # [B] -> [B, 1] -> [maxk, B]
         # compare every topk's model prediction with the ground truth & give credit if any matches the ground truth
         correct = (y_pred == target_reshaped)  # [maxk, B] were for each example we know which topk prediction matched truth
