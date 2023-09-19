63363d629b644b2e4a1be6cdbbb66859b7841213
Kaiwen Yang <92202424+kai-wen-yang@users.noreply.github.com>
2023-09-19 21:35:39 +0800

    Update my_eval.py

--- my_eval.py
+++ my_eval.py
@@ -146,6 +146,14 @@ def main(args):
         logits_base = image_features @ zeroshot_weights_base
         outputs.append(logits_base.cpu())
         targets.append(target.cpu())
+	    
+	_, y_pred = logits_base.topk(k=5, dim=1)
+        questions=[]
+	pdb.set_trace()
+	for i in range(targets.size(0)):
+	     options = '\n- '.join([openai_classnames[ind] for ind in y_pred[i].tolist()])
+	     questions.append("What is the object in the image?\nChoose the best answer from the following choices:\n- {options}")
+		
     acc=accuracy(torch.cat(outputs,dim=0), torch.cat(targets,dim=0), (1,5))
     print(acc)
 	
