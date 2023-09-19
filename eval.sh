3c292f013f29aaf1e25df7dfc14634550a4286fa
Kaiwen Yang <92202424+kai-wen-yang@users.noreply.github.com>
2023-09-20 01:17:23 +0800

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -11,7 +11,7 @@ cd ..
 #--cot 'Let us think step by step. '
 
 python eval.py --model_name InstructBLIP --batch_size 16 --eval_cls_clip --dataset_name ImageNet --device 0 --sample_num 1000 --max_new_tokens 64 \
---cot 'Sub-question 1: What is the primary color of the object?\nSub-question 2: What is the shape of the object?\nSub-question 3: Is the object typically found indoors or outdoors?\nSub-question 4: Does the object have any text or writing on it?\n '
+--cot 'Sub-question 1: What is the primary color of the object?\nSub-question 2: What is the shape of the object?\nSub-question 3: Is the object typically found indoors or outdoors?\n '
 #python eval.py --model_name InstructBLIP --batch_size 16 --eval_cls --dataset_name ImageNet --device 0 --sample_num 1000
 
 #python eval.py --model_name InstructBLIP --batch_size 2 --eval_vqa --dataset_name VCR1_MCI --device 0 --sample_num 1000
