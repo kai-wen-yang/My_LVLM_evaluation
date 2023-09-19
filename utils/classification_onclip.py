433be7992e8dd0c54f566eac56cff115d3cac7fe
Kaiwen Yang <92202424+kai-wen-yang@users.noreply.github.com>
2023-09-19 23:35:29 +0800

    Update classification_onclip.py

--- utils/classification_onclip.py
+++ utils/classification_onclip.py
@@ -51,7 +51,6 @@ def evaluate_zero_shot_image_classification_clip(
              options = '\n- '.join([openai_classnames[ind] for ind in y_pred[i].tolist()])
              questions.append(f"What is the object in the image?")#\nChoose the best answer from the following choices:\n- {options}")	
         ####
-        pdb.set_trace()
         outputs = model.batch_generate(batch['image_path'], questions, max_new_tokens=max_new_tokens)
         j = 0
         for image_path, gt_answer, output in zip(batch['image_path'], batch['gt_answers'], outputs):
